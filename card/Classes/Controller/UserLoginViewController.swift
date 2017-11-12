a987d1f402a295d5705101425ef833b6885effe6
yihui <huig@yihuideMacBook-Pro.local>
2017-11-12 11:34:45 +0800

    登录界面代码优化

--- card/Classes/Controller/UserLoginViewController.swift
+++ card/Classes/Controller/UserLoginViewController.swift
@@ -43,7 +43,9 @@ class UserLoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 加载UI
         setuoUI()
+        // 加载通知
         setupNotification()
         
     }
@@ -86,18 +88,16 @@ class UserLoginViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        
+        // 加载数据
         loadData()
         // 上移隐藏icon
         self.bottomIconImage.constant += 450
-        
-        // 设置按钮透明
+        // 设置登录按钮透明
         loginButton.alpha = 0
-        // 问题
+        // 隐藏问题
         centerProblem.constant = -view.bounds.width
-        // 答案
+        // 隐藏答案
         centerAnswer.constant = -view.bounds.width
-
         // 左移动隐藏密码输入框
         centerAlignPass.constant -= view.bounds.width
         
@@ -107,7 +107,7 @@ class UserLoginViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // 如果没有设置过密码修改按钮显示问题
+        // 如果没有设置过密码显示设置密码 UI
         if pass?.count == nil || (pass?.count)! < 1 {
             problemField.isHidden = false
             answerfield.isHidden = false
@@ -115,7 +115,7 @@ class UserLoginViewController: UIViewController {
             buttonBottomConstraint.constant = 60
             loginButton.setTitle("第一次使用请先设置密码", for: .normal)
             
-        } else {
+        } else {// 有密码显示登录 UI
 
             buttonTopConstraint.constant = 10
             buttonBottomConstraint.constant = 150
@@ -127,7 +127,7 @@ class UserLoginViewController: UIViewController {
         
     }
     
-    //MARK: -- UI动画
+    //MARK: -- 登录界面UI动画
     private func setupAnimate() {
         // icon动画
         UIView.animate(withDuration: 2.0, delay: 0.1, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
@@ -142,7 +142,6 @@ class UserLoginViewController: UIViewController {
             
         }, completion:{ (_) in
            
-            
             if self.pass?.count == nil || (self.pass?.count)! < 1 {
                 
                 // 问题输入框动画
@@ -169,23 +168,24 @@ class UserLoginViewController: UIViewController {
             
         }, completion: nil)
         
-
-        
     }
     
+    
+    // MARK: -- 找回密码按钮 点击事件
+    // 按钮Tag值
     var passRetrievalTag = 1
     
     @IBAction func passRetrievalClick(_ sender: Any) {
         
-        if passRetrievalTag == 1 {
+        if passRetrievalTag == 1 { // 找回密码问题验证
             
             passField.isHidden = true
             problemField.isHidden = false
             answerfield.isHidden = false
             problemField.isEnabled = false
             problemField.text = pass?.last?.problem
-            self.centerProblem.constant += self.view.bounds.width
-            self.centerAnswer.constant += self.view.bounds.width
+            centerProblem.constant += self.view.bounds.width
+            centerAnswer.constant += self.view.bounds.width
             problemTopConstraint.constant = -30
             buttonTopConstraint.constant = 50
             buttonBottomConstraint.constant = 100
@@ -197,23 +197,22 @@ class UserLoginViewController: UIViewController {
             self.view.layoutIfNeeded()
             return
             
-        } else if passRetrievalTag == 2 {
+        } else if passRetrievalTag == 2 { // 还原登录UI
             
             askButton.setTitle("?", for: .normal)
             passField.isHidden = false
-            self.viewWillAppear(true)
-            self.viewDidAppear(true)
+            viewWillAppear(true)
+            viewDidAppear(true)
             passTag = 0
             passRetrievalTag = 1
-            self.view.layoutIfNeeded()
+            view.layoutIfNeeded()
             return
         }
         
     }
     
-    
+    // MAKE: -- 问题,答案输入框动画
     func animAsk() {
-        
         problemField.alpha = 0
         answerfield.alpha = 0
         loginButton.alpha = 0
@@ -232,97 +231,83 @@ class UserLoginViewController: UIViewController {
     }
     
     var passTag = 0
-    
     //MARK: -- 按钮点击事件
     @IBAction func loginClick(_ sender: Any) {
+        
+        if passTag == 0 {
+            // 登录验证
+            loginCheck()
+        }
+        if passTag == 1 || passTag == 2 {
+            // 找回更新密码
+            updatePass()
+        }
+    }
     
+    // MARK: -- 找回更新密码
+    private func updatePass() {
+        // 验证输入
+        guard let answerText = answerfield.text, answerfield.text != ""  else {
+            Tools.show(self, message: "答案不能空!")
+            return
+        }
         // 找回密码
         if passTag == 1 {
-            guard let answerText = answerfield.text, answerfield.text != ""  else {
-                Tools.show(self, message: "答案不能空!")
-                return
-            }
-            
+            // 验证答案是否正确
             if pass?.last?.answer != answerText {
                 Tools.show(self, message: "答案错误!")
                 return
             }
+            // 答案正确 修改UI,显示设置新密码UI
             passField.isHidden = false
             problemTopConstraint.constant = 10
             buttonTopConstraint.constant = 90
             buttonBottomConstraint.constant = 60
             loginButton.setTitle("确定修改", for: .normal)
             passField.placeholder = "请输入新密码!"
+            // 刷新UI界面
             self.view.layoutIfNeeded()
-            
             passTag = 2
             return
         }
         
+        // 验证输入
         guard let tempPass = passField.text, passField.text != "" else {
             passField.text = nil
             Tools.show(self, message: "请输入密码!")
             return
         }
         
+        // 更新密码
         if passTag == 2 {
-            
-            guard let answerText = answerfield.text, answerfield.text != ""  else {
-                Tools.show(self, message: "答案不能空!")
-                return
-            }
-            
+            // 保存密码
             BankData(passMd5(tempPass),problem: (pass?.last?.problem)!,answer: answerText).updateData(type: .Pass)
-            
+            // 还原登录UI
             passTag = 0
             askButton.setTitle("?", for: .normal)
-
+            passField.placeholder = "请输入密码!"
+            // 清空输入框
             problemField.text = nil
             answerfield.text = nil
-            Tools.show(self, message: "成功修改资料!")
-            
-            self.viewWillAppear(true)
-            self.viewDidAppear(true)
-            
-            return
-        }
-        
-        // 保存密码,问题和答案
-        depositUserData(tempPass)
-        
-        // 登录验证
-        loginCheck(tempPass)
-        
-    }
-    // MARK: -- 保存密码,问题和答案
-    private func depositUserData(_ tempPass: String) {
-        
-        if pass?.count == nil || (pass?.count)! < 1 {
-            
-            guard let problemText = problemField.text, problemField.text != "" else {
-                Tools.show(self, message: "问题不能空!")
-                return
-            }
-            
-            guard let answerText = answerfield.text, answerfield.text != ""  else {
-                Tools.show(self, message: "答案不能空!")
-                return
-            }
-            
-            BankData(passMd5(tempPass),problem: problemText,answer: answerText).insertData(type: .Pass)
-            
             passField.text = nil
-            problemField.text = nil
-            answerfield.text = nil
-            view.endEditing(true)
-            
-            self.performSegue(withIdentifier: "Home", sender: nil)
+            // 提示
+            Tools.show(self, message: "成功修改资料!")
+            // 重新加载页面
+            viewWillAppear(true)
+            viewDidAppear(true)
             return
         }
     }
     
-    // MARK: -- 登录验证
-    private func loginCheck(_ tempPass: String) {
+    // MARK: -- 登录验证,保存密码,问题和答案
+    private func loginCheck() {
+        
+        // 验证输入
+        guard let tempPass = passField.text, passField.text != "" else {
+            passField.text = nil
+            Tools.show(self, message: "请输入密码!")
+            return
+        }
         
         if pass?.count == 1 { // 有设置过密码判断密码是否正确
             
@@ -338,9 +323,34 @@ class UserLoginViewController: UIViewController {
                 Tools.show(self, message: "密码错误!")
                 passField.text = nil
             }
+            
+            return
+            
+        }  else if pass?.count == nil || (pass?.count)! < 1 {
+            // 输入框验证
+            guard let problemText = problemField.text, problemField.text != "" else {
+                Tools.show(self, message: "问题不能空!")
+                return
+            }
+            guard let answerText = answerfield.text, answerfield.text != ""  else {
+                Tools.show(self, message: "答案不能空!")
+                return
+            }
+            // 保存资料到数据库
+            BankData(passMd5(tempPass),problem: problemText,answer: answerText).insertData(type: .Pass)
+            // 清空输入框
+            passField.text = nil
+            problemField.text = nil
+            answerfield.text = nil
+            // 关闭键盘
+            view.endEditing(true)
+            // 直接进入主页
+            self.performSegue(withIdentifier: "Home", sender: nil)
+            
+            return
         }
         
-        return
+        
     }
     
     // MARK: -- MD5加密
@@ -349,12 +359,14 @@ class UserLoginViewController: UIViewController {
         return tempPass.md5()
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     deinit {
+        // 删除通知
         NotificationCenter.default.removeObserver(self)
         HHLog("销毁")
     }
